6276478116e12a1731d203b2ab3427f03e0bbc4c
Deborah Hamel <deborahleehamel@gmail.com>
2016-06-13 17:09:41 -0600

    Implemented oldest and youngest student.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,11 @@
 class Teacher < ActiveRecord::Base
   has_many :students
+
+  def oldest_student
+    students.maximum(:age)
+  end
+
+  def youngest_student
+    students.minimum(:age)
+  end
 end
---
+++ test/integration/visitor_sees_oldest_and_youngest_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+  class VisitorCanSeeTeachersTest < ActionDispatch::IntegrationTest
+    test "visitor sees teachers list with students" do
+
+      #setup
+      #create 3 teachers
+      teacher1 = Teacher.create(first_name: "Billy", last_name: "Jeans")
+      teacher2 = Teacher.create(first_name: "Hilary", last_name: "Clinton")
+      teacher3 = Teacher.create(first_name: "Jill", last_name: "Stein")
+
+
+      #create students for 3 teachers
+      teacher1.students << [Student.create(first_name: "Heidi", last_name: "Jeans", age: 20), Student.create(first_name: "Billy", last_name: "Jeans", age: 30), Student.create(first_name: "Larry", last_name: "Jeans", age: 25), Student.create(first_name: "Bobby", last_name: "Jeans", age: 19)]
+      teacher2.students << Student.create(first_name: "Deb", last_name: "Jeans", age: 40)
+      teacher3.students << [Student.create(first_name: "Jim", last_name: "Jeans", age: 50), Student.create(first_name: "Allan", last_name: "Jeans", age: 70)]
+
+
+      #visitor goes to /teachers
+      visit '/teachers'
+      save_and_open_page
+
+      #teacher's oldest and youngest student age shows on page
+      assert page.has_content? "Oldest student: 30"
+      assert page.has_content? "Youngest student: 19"
+    end
+
+end
